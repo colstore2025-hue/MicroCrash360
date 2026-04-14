2b52cb0e753c3555ce674f7778b588e2aacaf439
Easy Vehicle Usa <col.store2025@gmail.com>
2026-04-13 20:21:35 -0500

    Crear loginUI.js

---
+++ ui/loginUI.js
@@ -0,0 +1,9 @@
+export function renderLogin() {
+  return `
+    <div class="screen">
+      <h1>MicroCash360</h1>
+      <input id="phone" placeholder="Número celular"/>
+      <button id="loginBtn">Continuar</button>
+    </div>
+  `;
+}
